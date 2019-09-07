2e27704bf7ce98c8fc34b34878bc7fc16cd17cd0
nshCore <nshcore@protonmail.com>
2019-09-07 14:23:22 +0100

    init commit

---
+++ packages/protocol-sdk-js/README.md
@@ -0,0 +1,1 @@
+# protocol-sdk-js
---
+++ packages/protocol-sdk-js/src/signaturetype.js
@@ -0,0 +1,22 @@
+/**
+ * @fileoverview
+ * @enhanceable
+ * @suppress {messageConventions} JS Compiler reports an error if a variable or
+ *     field starts with 'MSG_' and isn't a translatable message.
+ * @public
+ */
+// GENERATED CODE -- DO NOT EDIT!
+
+goog.provide('proto.Catalyst.Protocol.Common.SignatureType');
+
+/**
+ * @enum {number}
+ */
+proto.Catalyst.Protocol.Common.SignatureType = {
+  SIGNATURE_TYPE_UNKNOWN: 0,
+  TRANSACTION_PUBLIC: 1,
+  TRANSACTION_CONFIDENTIAL: 2,
+  PROTOCOL_RPC: 3,
+  PROTOCOL_PEER: 4
+};
+
