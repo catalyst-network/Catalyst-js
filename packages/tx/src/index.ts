ba94fd97e47f93e64bb19d3331aaf57357cf0610
stephenhorsfall <stephen.horsfall@atlascity.io>
2020-01-15 17:54:10 +0000

    remove multiple entries

--- packages/tx/src/index.ts
+++ packages/tx/src/index.ts
@@ -10,66 +10,17 @@ async function loadWasm() {
   return import('wasm-ed25519ph');
 }
 
-
-
-// export function signMessage(message: any, pKey: any): Uint8Array {
-//   return nacl.sign(message, pKey);
-// }
-
-// async function transaction(wallet: any, txParams: any): Promise<Uint8Array> {
-//   if(!txParams.value) {
-//     txParams.value = '0x0';
-//   }
-
-//   if(!txParams.to) {
-//     txParams.to = '';
-//   }
-
-//   const base = new BaseEntry();
-//   base.setNonce(parseInt(txParams.nonce, 16));
-//   base.setReceiverPublicKey(bytesFromHexString(txParams.to));
-//   base.setSenderPublicKey(wallet.publicKey);
-//   base.setTransactionFees(new Uint8Array(8));
-
-//   const entry = new PublicEntry();
-//   entry.setBase(base);
-//   entry.setAmount(bytesFromHexString(txParams.value));
-//   entry.setData(bytesFromHexString(txParams.data));
-//   entry.setTimestamp();
-//   entry.setGasPrice(bytesFromHexString(txParams.gasPrice));
-//   entry.setGasLimit(parseInt(txParams.gas, 16));
-
-//   const context = new SigningContext();
-//   context.setNetworkType(NetworkType.TESTNET);
-//   context.setSignatureType(SignatureType.TRANSACTION_PUBLIC);
-
-//   const wasm = await loadWasm();
-//   const sig = signTx(entry.serializeBinary(), wallet, context.serializeBinary(), wasm);
-
-//   const signature = new Signature();
-//   signature.setSigningContext(context);
-//   signature.setRawBytes(sig);
-
-//   entry.setSignature(signature);
-
-//   const broadcast = new TransactionBroadcast();
-//   broadcast.setPublicEntry(entry);
-  
-//   return broadcast.serializeBinary();
-// }
-
 export default class Transaction {
-  txs: TxData[]
-  entries: PublicEntry[]
+  tx: TxData
+  entry: PublicEntry
 
-
-  constructor(entries: TxData[] = []) {
-    this.txs = entries;
-    this._createTxEntries();
+  constructor(entry: TxData = {}) {
+    this.tx = entry;
+    this._createTxEntry();
   }
 
-  private _createTxEntries() {
-    this.txs.forEach((tx) => {
+  private _createTxEntry() {
+    const { tx } = this
       if(!tx.value) {
         tx.value = '0x0';
       }
@@ -84,15 +35,13 @@ export default class Transaction {
     // base.setSenderPublicKey(wallet.publicKey);
     base.setTransactionFees(new Uint8Array(8));
 
-    const entry = new PublicEntry();
-    entry.setBase(base);
-    entry.setAmount(bytesFromHexString(tx.value));
-    entry.setData(bytesFromHexString(tx.data));
-    entry.setTimestamp();
-    entry.setGasPrice(bytesFromHexString(tx.gasPrice));
-    entry.setGasLimit(parseInt(tx.gas, 16));
-    this.entries.push(entry);
-    });
+    this.entry = new PublicEntry();
+    this.entry.setBase(base);
+    this.entry.setAmount(bytesFromHexString(tx.value));
+    this.entry.setData(bytesFromHexString(tx.data));
+    this.entry.setTimestamp();
+    this.entry.setGasPrice(bytesFromHexString(tx.gasPrice));
+    this.entry.setGasLimit(parseInt(tx.gas, 16));
   }
 
   private _getPublicKey(privateKey: Uint8Array) {
@@ -104,17 +53,17 @@ export default class Transaction {
     const broadcast = new TransactionBroadcast();
 
     const wasm = await loadWasm();
-    this.entries.forEach((entry: PublicEntry) => {
-      const context = new SigningContext();
-      context.setNetworkType(NetworkType.TESTNET);
-      context.setSignatureType(SignatureType.TRANSACTION_PUBLIC);
-      const sig = this._signTx(entry.serializeBinary(), privateKey, context.serializeBinary(), wasm);
-      const signature = new Signature();
-      signature.setSigningContext(context);
-      signature.setRawBytes(sig);
-      entry.setSignature(signature);
-      broadcast.setPublicEntry(entry);
-    });
+    const { entry } = this;
+    const context = new SigningContext();
+    context.setNetworkType(NetworkType.TESTNET);
+    context.setSignatureType(SignatureType.TRANSACTION_PUBLIC);
+    const sig = this._signTx(entry.serializeBinary(), privateKey, context.serializeBinary(), wasm);
+    const signature = new Signature();
+    signature.setSigningContext(context);
+    signature.setRawBytes(sig);
+    entry.setSignature(signature);
+    broadcast.setPublicEntry(entry);
+    
     return broadcast.serializeBinary();
   }
 
