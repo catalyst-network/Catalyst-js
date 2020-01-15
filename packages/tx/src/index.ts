dbc6dfa1dedbdd9a58a3e8fa9eb9fbe3df80ef97
stephenhorsfall <stephen.horsfall@atlascity.io>
2020-01-15 16:41:32 +0000

    add transaction class and types

--- packages/tx/src/index.ts
+++ packages/tx/src/index.ts
@@ -1,74 +1,131 @@
-import * as nacl from 'tweetnacl';
+// import * as nacl from 'tweetnacl';
 import { BaseEntry, PublicEntry } from './protobuffs/Transaction_pb';
 import { Signature, SigningContext, SignatureType } from './protobuffs/Cryptography_pb';
 import { NetworkType } from './protobuffs/Network_pb';
 import { TransactionBroadcast } from './protobuffs/Wire_pb';
 import { bytesFromHexString } from './utils/index';
+import { TxData } from './types';
 
 async function loadWasm() {
   return import('wasm-ed25519ph');
 }
 
-function signTx(tx: any, wallet: any, context: Uint8Array, wasm: typeof import('wasm-ed25519ph')) {
-  const contextLength = context.length;
-  const signature = new Uint8Array(64);
-  const result = wasm.sign(
-    signature,
-    wallet.privateKey,
-    tx,
-    context,
-    contextLength,
-  );
-  if (result !== 418) {
-    throw Error('Failed to sign');
-  }
-  return signature;
-}
 
-export function signMessage(message: any, pKey: any): Uint8Array {
-  return nacl.sign(message, pKey);
-}
 
-async function transaction(wallet: any, txParams: any): Promise<Uint8Array> {
-  if(!txParams.value) {
-    txParams.value = '0x0';
-  }
+// export function signMessage(message: any, pKey: any): Uint8Array {
+//   return nacl.sign(message, pKey);
+// }
 
-  if(!txParams.to) {
-    txParams.to = '';
-  }
+// async function transaction(wallet: any, txParams: any): Promise<Uint8Array> {
+//   if(!txParams.value) {
+//     txParams.value = '0x0';
+//   }
 
-  const base = new BaseEntry();
-  base.setNonce(parseInt(txParams.nonce, 16));
-  base.setReceiverPublicKey(bytesFromHexString(txParams.to));
-  base.setSenderPublicKey(wallet.publicKey);
-  base.setTransactionFees(new Uint8Array(8));
+//   if(!txParams.to) {
+//     txParams.to = '';
+//   }
 
-  const entry = new PublicEntry();
-  entry.setBase(base);
-  entry.setAmount(bytesFromHexString(txParams.value));
-  entry.setData(bytesFromHexString(txParams.data));
-  entry.setTimestamp();
-  entry.setGasPrice(bytesFromHexString(txParams.gasPrice));
-  entry.setGasLimit(parseInt(txParams.gas, 16));
+//   const base = new BaseEntry();
+//   base.setNonce(parseInt(txParams.nonce, 16));
+//   base.setReceiverPublicKey(bytesFromHexString(txParams.to));
+//   base.setSenderPublicKey(wallet.publicKey);
+//   base.setTransactionFees(new Uint8Array(8));
 
-  const context = new SigningContext();
-  context.setNetworkType(NetworkType.TESTNET);
-  context.setSignatureType(SignatureType.TRANSACTION_PUBLIC);
+//   const entry = new PublicEntry();
+//   entry.setBase(base);
+//   entry.setAmount(bytesFromHexString(txParams.value));
+//   entry.setData(bytesFromHexString(txParams.data));
+//   entry.setTimestamp();
+//   entry.setGasPrice(bytesFromHexString(txParams.gasPrice));
+//   entry.setGasLimit(parseInt(txParams.gas, 16));
 
-  const wasm = await loadWasm();
-  const sig = signTx(entry.serializeBinary(), wallet, context.serializeBinary(), wasm);
+//   const context = new SigningContext();
+//   context.setNetworkType(NetworkType.TESTNET);
+//   context.setSignatureType(SignatureType.TRANSACTION_PUBLIC);
 
-  const signature = new Signature();
-  signature.setSigningContext(context);
-  signature.setRawBytes(sig);
+//   const wasm = await loadWasm();
+//   const sig = signTx(entry.serializeBinary(), wallet, context.serializeBinary(), wasm);
 
-  entry.setSignature(signature);
+//   const signature = new Signature();
+//   signature.setSigningContext(context);
+//   signature.setRawBytes(sig);
 
-  const broadcast = new TransactionBroadcast();
-  broadcast.setPublicEntry(entry);
+//   entry.setSignature(signature);
+
+//   const broadcast = new TransactionBroadcast();
+//   broadcast.setPublicEntry(entry);
   
-  return broadcast.serializeBinary();
-}
+//   return broadcast.serializeBinary();
+// }
 
-export default transaction;
+export default class Transaction {
+  txs: TxData[]
+  entries: PublicEntry[]
+
+
+  constructor(entries: TxData[] = []) {
+    this.txs = entries;
+    this._createTxEntries();
+  }
+
+  private _createTxEntries() {
+    this.txs.forEach((tx) => {
+      if(!tx.value) {
+        tx.value = '0x0';
+      }
+    
+      if(!tx.to) {
+        tx.to = '';
+      }
+
+    const base = new BaseEntry();
+    base.setNonce(parseInt(tx.nonce, 16));
+    base.setReceiverPublicKey(bytesFromHexString(tx.to));
+    // base.setSenderPublicKey(wallet.publicKey);
+    base.setTransactionFees(new Uint8Array(8));
+
+    const entry = new PublicEntry();
+    entry.setBase(base);
+    entry.setAmount(bytesFromHexString(tx.value));
+    entry.setData(bytesFromHexString(tx.data));
+    entry.setTimestamp();
+    entry.setGasPrice(bytesFromHexString(tx.gasPrice));
+    entry.setGasLimit(parseInt(tx.gas, 16));
+    this.entries.push(entry);
+    });
+  }
+
+  async sign(privateKey: Buffer, opt?: object){
+    const broadcast = new TransactionBroadcast();
+
+    const wasm = await loadWasm();
+    this.entries.forEach((entry: PublicEntry) => {
+      const context = new SigningContext();
+      context.setNetworkType(NetworkType.TESTNET);
+      context.setSignatureType(SignatureType.TRANSACTION_PUBLIC);
+      const sig = this._signTx(entry.serializeBinary(), privateKey, context.serializeBinary(), wasm);
+      const signature = new Signature();
+      signature.setSigningContext(context);
+      signature.setRawBytes(sig);
+      entry.setSignature(signature);
+      broadcast.setPublicEntry(entry);
+    });
+    return broadcast.serializeBinary();
+  }
+
+  private _signTx(tx: any, privateKey: any, context: Uint8Array, wasm: typeof import('wasm-ed25519ph')) {
+    const contextLength = context.length;
+    const signature = new Uint8Array(64);
+    const result = wasm.sign(
+      signature,
+      privateKey,
+      tx,
+      context,
+      contextLength,
+    );
+    if (result !== 418) {
+      throw Error('Failed to sign');
+    }
+    return signature;
+  }
+}
---
+++ packages/tx/src/types.ts
@@ -0,0 +1,49 @@
+/**
+ * Any object that can be transformed into a `Buffer`
+ */
+export interface TransformableToBuffer {
+    toBuffer(): Buffer
+  }
+  
+/**
+ * A hex string prefixed with `0x`.
+ */
+export type PrefixedHexString = string
+
+/**
+ * A Buffer, hex string prefixed with `0x`, Number, or an object with a toBuffer method such as BN.
+ */
+export type BufferLike = Buffer | TransformableToBuffer | PrefixedHexString | number
+
+export type numOrString = string | number
+
+export interface TxData {
+    /**
+     * The transaction's gas limit.
+     */
+    gas?: string
+  
+    /**
+     * The transaction's gas price.
+     */
+    gasPrice?: string
+  
+    /**
+     * The transaction's the address is sent to.
+     */
+    to?: string
+  
+    /**
+     * The transaction's nonce.
+     */
+    nonce?: string
+    /**
+     * This will contain the data of the message or the init of a contract
+     */
+    data?: string
+  
+    /**
+     * The amount of Ether sent.
+     */
+    value?: string
+  }
