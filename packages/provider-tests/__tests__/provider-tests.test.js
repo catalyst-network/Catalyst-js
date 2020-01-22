697c78d51d4f93cdc9a273e02b7cba620a00d18c
stephenhorsfall <stephen.horsfall@atlascity.io>
2020-01-22 14:09:49 +0000

    remove unused package

--- packages/provider-tests/__tests__/provider-tests.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const providerTests = require('..');
-
-describe('provider-tests', () => {
-    it('needs tests');
-});
--- packages/provider-tests/lib/provider-tests.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = providerTests;
-
-function providerTests() {
-    // TODO
-}
