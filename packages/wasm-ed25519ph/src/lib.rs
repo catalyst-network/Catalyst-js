6cbc68c103676594ca7a8f072d287aea28c95bcc
fran <fran.sl@atlascity.io>
2020-02-06 11:41:42 +0000

    Bodge to create 64 byte array from slice without allocations, until const generics implemented fully

--- packages/wasm-ed25519ph/src/lib.rs
+++ packages/wasm-ed25519ph/src/lib.rs
@@ -1,12 +1,9 @@
-extern crate catalyst_protocol_sdk_rust;
+#![feature(const_generics)]
 extern crate console_error_panic_hook;
 extern crate web_sys;
 
-use catalyst_protocol_sdk_rust::prelude::*;
-use catalyst_protocol_sdk_rust::Cryptography::ErrorCode;
-
-use ed25519_dalek::{Digest, Sha512};
-use ed25519_dalek::{Keypair, PublicKey, SecretKey, Signature};
+use catalyst_common::*;
+use std::convert::TryInto;
 
 use std::slice;
 use wasm_bindgen::prelude::*;
@@ -16,10 +13,16 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-pub fn public_key_from_private(private_key: &[u8]) -> Vec<u8> {
-    let secret = SecretKey::from_bytes(private_key).unwrap();
-    let public: PublicKey = (&secret).into();
-    public.to_bytes().to_vec()
+pub fn public_key_from_private(public_key: &mut [u8], private_key: &[u8]) -> i32 {
+    let private_key = match private_key.try_into(){
+        Ok(private_key) => private_key,
+        Err(_) => return ErrorCode::INVALID_PRIVATE_KEY.value()
+    };
+    let public_key = match public_key.try_into(){
+        Ok(public_key) => public_key,
+        Err(_) => return ErrorCode::INVALID_PUBLIC_KEY.value()
+    };
+    keys::publickey_from_private(public_key, private_key)
 }
 
 #[wasm_bindgen]
@@ -27,35 +30,32 @@ pub fn init_panic_hook() {
     console_error_panic_hook::set_once();
 }
 
+
 #[wasm_bindgen]
 pub fn sign(
     signature: &mut [u8],
+    public_key: &mut [u8],
     private_key: &[u8],
     message: &[u8],
     context: &[u8],
     context_length: usize,
 ) -> i32 {
-    let private_key = match SecretKey::from_bytes(private_key) {
+    let private_key = match private_key.try_into(){
         Ok(private_key) => private_key,
-        Err(_) => return ErrorCode::INVALID_PRIVATE_KEY.value(),
+        Err(_) => return ErrorCode::INVALID_PRIVATE_KEY.value()
     };
-    let public_key = PublicKey::from(&private_key);
-    let keypair: Keypair = Keypair {
-        secret: private_key,
-        public: public_key,
+    let public_key = match public_key.try_into(){
+        Ok(public_key) => public_key,
+        Err(_) => return ErrorCode::INVALID_PUBLIC_KEY.value()
+    };
+    let signature = match try_from_sig_slice(signature){
+        Ok(signature) => signature,
+        Err(_) => return ErrorCode::INVALID_SIGNATURE.value()
     };
     let context = unsafe { slice::from_raw_parts(context.as_ptr(), context_length) };
-
-    if context.len() > 255 || context.len() != context_length {
-        return ErrorCode::INVALID_CONTEXT_LENGTH.value();
-    }
-    let mut prehashed: Sha512 = Sha512::new();
-    prehashed.input(message);
-    let sig = keypair.sign_prehashed(prehashed, Some(context)).to_bytes(); //need to handle errors here
-    signature.copy_from_slice(&sig);
-    ErrorCode::NO_ERROR.value()
+    std_signature::sign(signature, public_key, private_key, message, context)
 }
-
+/*
 #[wasm_bindgen]
 pub fn verify(
     signature: &[u8],
@@ -118,7 +118,7 @@ pub fn sign_sig_and_public_key(
 
     ErrorCode::NO_ERROR.value()
 }
-
+*/
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -131,14 +131,16 @@ mod tests {
 
     #[wasm_bindgen_test]
     fn can_create_signature() {
-        let private: [u8; 32] = [0; 32];
+        let private_key: [u8; 32] = [0; 32];
+        let mut public_key : [u8; 32] = [0; 32];
+        keys::publickey_from_private(&mut public_key, &private_key);
         let message = b"message";
         let context = b"context";
         let mut sig: [u8; 64] = [0; 64];
-        let result = sign(&mut sig, &private, message, context, context.len());
+        let result = sign(&mut sig, &mut public_key, &private_key, message, context, context.len());
         assert_eq!(result, ErrorCode::NO_ERROR.value());
 	}
-	
+	/*
 	#[wasm_bindgen_test]
     fn can_validate_signature() {
 		let private: [u8; 32] = [0; 32];
@@ -164,5 +166,17 @@ mod tests {
 		sign(&mut sig, &private, message, context, context.len());
 		let result = verify(&sig, &public_bytes, message, context2, context2.len());
         assert_eq!(result, ErrorCode::SIGNATURE_VERIFICATION_FAILURE.value());
+    }
+    */
+}
+
+
+fn try_from_sig_slice(slice: &mut [u8]) -> Result<&mut [u8; constants::SIGNATURE_LENGTH], ErrorCode> {
+    if slice.len() == constants::SIGNATURE_LENGTH {
+        let ptr = slice.as_mut_ptr() as *mut [u8; constants::SIGNATURE_LENGTH];
+        // SAFETY: ok because we just checked that the length fits
+        unsafe { Ok(&mut *ptr) }
+    } else {
+        Err(ErrorCode::INVALID_SIGNATURE)
     }
 }
